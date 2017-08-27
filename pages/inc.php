67bc2ca086abd6064ce22e4e35663d89a18c83c6
Dane Watkins <dane.watkins@gmail.com>
2017-08-27 18:56:43 +0100

    cursor, remove errors in inc

--- pages/inc.php
+++ pages/inc.php
@@ -1,16 +1,36 @@
 <?php
 // var_dump($_POST);
+session_start();
+$_SESSION["under16"]="";
 if(isset($_POST["page"])){
 	$posted_page = $_POST["page"];
 }else{
 	$posted_page=0;
 }
+if ($posted_page==2){
+	$answer=explode(",",$_POST["answer"]);
+	if($answer[0]<206){
+		$_SESSION["under16"]="under";
+	}else{
+		$_SESSION["under16"]="over";
+	}
+}
+
 
 if(isset($_POST["answer"])){
-	$posted_answer= $_POST["answer"];
+	if($_SESSION["under16"]=="under"){
+		$posted_answer="under 16";
+	}else{
+		$posted_answer= $_POST["answer"];
+	}
 }else{
 	$posted_answer="0";
 }
+
+if ($posted_page==1){
+	session_destroy();
+}
+#var_dump ($_SESSION);
 date_default_timezone_set('Europe/London');
 $file = "../answers/answers-".date('Y-m-d-H').".txt";
 $date=date('Y-m-d H:i:s');
--- scripts/temp.sh
+++ scripts/temp.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-#get the temperature of the pi and save it to answeres folder
 
-echo $(date)  "," $(/opt/vc/bin/vcgencmd measure_temp) >> ../answers/temp.txt
+#get the temperature of the pi and save it to answers folder
+
+echo $(date)  "," $(/opt/vc/bin/vcgencmd measure_temp) >> /var/www/html/arnolfini-survey/answers/temp.txt
